08d143794ad9ea20ba4a0158cdd439454d90d4c6
Dongeun Kim <boramatlulu@gmail.com>
2021-01-27 22:30:41 +0900

    About 페이지 완료

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # 개발자 김동은 블로그
 웹 주소: https://ggimdong.github.io/
-HTML, JS, CSS만 가지고 바닥부터 제작한 웹사이트입니다.
--- js/warning.js
+++ js/warning.js
@@ -1,5 +1,5 @@
 var msg = '<div class=\"header\"><a id=\"close\" href="#">닫기 X</a></div>';
-msg += '<div class=\"message\"><h2>아직 미완성된 상태입니다.<h2>';
+msg += '<div class=\"message\"><h2>여기는 아직 미완성된 상태입니다.<h2>';
 msg += '빠른 시일 내에 완성하도록 하겠습니다. ㅎㅎ...<div>';
 
 var elNote = document.createElement('div');
