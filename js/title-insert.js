caa986159e41de68660abc9e9ca19f9bee1efece
Dongeun Kim <boramatlulu@gmail.com>
2021-02-08 18:42:46 +0900

    Font 버그 수정

---
+++ js/title-insert.js
@@ -0,0 +1,7 @@
+/**************여기를 수정**********************/
+var title_msg = "개발자 김동은 블로그";
+var subtitle_msg = "~ 후론트 개발자는 내 부캐! ~";
+/**********************************************/
+
+document.getElementById('title').innerText = title_msg;
+document.getElementById('subtitle').innerText = subtitle_msg;
